f8447e755668f9b7bd79a5a1ec5991f0abaaeb2f
rinrin <r.chaaaaaan@gmail.com>
2022-08-31 17:53:38 +0900

    8-課題2

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz {
+  public static void main(String[] args){
+    int i = Integer.parseInt(args[0]);
+    for(int k = 1; k <= i; k++){
+     if(k % 3 == 0 && k % 5 == 0){
+         System.out.println("FizzBuzz");
+     }else if(k % 5 == 0){
+         System.out.println("Buzz");
+     }else if(k % 3 == 0){
+         System.out.println("Fizz");
+     }else{
+         System.out.println(k);
+     }
+    }
+
+  }
+}
+
+
