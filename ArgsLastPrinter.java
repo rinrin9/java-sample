bfd315d4d197e3bc4ef7083cecac1a6facdce827
rinrin <r.chaaaaaan@gmail.com>
2022-08-31 17:42:58 +0900

    8-課題1

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,19 @@
+public class ArgsLastPrinter{
+  public static void main(String[] args) {
+    int i = args.length;
+    if (args.length == 0) {
+      System.out.println("なし");
+    }else{       
+      System.out.println(args[i-1]);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
