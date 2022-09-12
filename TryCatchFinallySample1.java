e82ee7948c02bcd1f61a3805a52ec22b19c04d5f
rinrin <r.chaaaaaan@gmail.com>
2022-09-12 18:22:37 +0900

    9課題3

---
+++ TryCatchFinallySample1.java
@@ -0,0 +1,18 @@
+import java.util.logging.*;
+
+
+public class TryCatchFinallySample{
+    private static Logger logger = Logger.getLogger(TryCatchFinallySample.class.getName());
+    public static void main(String[] args) {
+		logger.setLevel(Level.ALL);
+
+        try {
+            logger.warning("try");
+            Integer.parseInt(args[0]);
+        }catch(Exception e){
+            logger.severe("catch");
+        }finally{
+            logger.info("finally");
+        }
+    }
+}
