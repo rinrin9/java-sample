4f068978ea5a6f1c60e56b834fa04f126c19fb41
rinrin <r.chaaaaaan@gmail.com>
2022-09-12 18:30:30 +0900

    10-課題2

---
+++ Person2.java
@@ -0,0 +1,30 @@
+public abstract class AbstractPerson {
+    public abstract String getGender();
+
+    public void speak() {
+        System.out.println("I'm a " + getGender());
+
+    }
+}
+
+public class Man extends AbstractPerson {
+    public String getGender() {
+        return MAN;
+    }
+}
+
+public class Woman extends AbstractPerson {
+    public String getGender() {
+        return WOMAN;
+    }
+}
+
+public class GenderSpeaker {
+    public static void main(String[] args) {
+        doSpeak(new Man());
+        doSpeak(new Woman());
+    }
+    private static void doSpeak(AbstractPerson person) {
+        person.speak();
+    }
+} 
