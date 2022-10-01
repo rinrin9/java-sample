de052867e06c0637c67909b9eaca905c9646a890
rinrin <r.chaaaaaan@gmail.com>
2022-10-01 15:44:41 +0900

    9-課題3
    Format

--- TryCatchFinallySample1.java
+++ TryCatchFinallySample1.java
@@ -1,18 +1,18 @@
 import java.util.logging.*;
 
+public class TryCatchFinallySample {
+	private static Logger logger = Logger.getLogger(TryCatchFinallySample.class.getName());
 
-public class TryCatchFinallySample{
-    private static Logger logger = Logger.getLogger(TryCatchFinallySample.class.getName());
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		logger.setLevel(Level.ALL);
 
-        try {
-            logger.warning("try");
-            Integer.parseInt(args[0]);
-        }catch(Exception e){
-            logger.severe("catch");
-        }finally{
-            logger.info("finally");
-        }
-    }
+		try {
+			logger.warning("try");
+			Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			logger.severe("catch");
+		} finally {
+			logger.info("finally");
+		}
+	}
 }
