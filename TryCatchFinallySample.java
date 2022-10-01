43688b5fa1be782cabeaa259508c4b6bc2300bb7
rinrin <r.chaaaaaan@gmail.com>
2022-10-01 15:43:23 +0900

    9-課題2
    Format

--- TryCatchFinallySample.java
+++ TryCatchFinallySample.java
@@ -1,12 +1,12 @@
-public class TryCatchFinallySample{
-    public static void main(String[] args){
-        try {
-            System.out.println("try");
-            Integer.parseInt(args[0]);
-        }catch(Exception e){
-            System.out.println("catch");
-        }finally{
-            System.out.println("finally");
-        }
-    }
+public class TryCatchFinallySample {
+	public static void main(String[] args) {
+		try {
+			System.out.println("try");
+			Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			System.out.println("catch");
+		} finally {
+			System.out.println("finally");
+		}
+	}
 }
