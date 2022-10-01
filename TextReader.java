d632f17ddfec4f65741cbab4143f7b4fbd97ce1c
rinrin <r.chaaaaaan@gmail.com>
2022-10-01 15:52:19 +0900

    10-課題3
    Format

--- TextReader.java
+++ TextReader.java
@@ -6,40 +6,39 @@ import java.util.List;
 import java.util.logging.*;
 
 public class TextReader {
-    private String path;
+	private String path;
 
-    private static Logger logger = Logger.getLogger(TextReader.class.getName());
+	private static Logger logger = Logger.getLogger(TextReader.class.getName());
 
-    private enum Open {
-        START, END 
-    }
+	private enum Open {
+		START, END
+	}
 
-    private TextReader(String path) {
-        this.path = path;
-    }
+	private TextReader(String path) {
+		this.path = path;
+	}
 
-    private void execute() {
-        List<Integer> valueList = new ObjectList<Integer>();
+	private void execute() {
+		List<Integer> valueList = new ObjectList<Integer>();
 
+		try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr);) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				valueList.add(Integer.parseInt(line));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(fr);) {                       
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                valueList.add(Integer.parseInt(line)); 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		for (Integer x : valueList) {
+			System.out.println(x);
+		}
+	}
 
-        for(Integer x: valueList){
-            System.out.println(x);
-        }
-    }
-
-    public static void main(String... args) {
-        logger.info(Open.START.name());
-        new TextReader(args[0]).execute();
-        logger.info(Open.END.name());
-    }
+	public static void main(String... args) {
+		logger.info(Open.START.name());
+		new TextReader(args[0]).execute();
+		logger.info(Open.END.name());
+	}
 
 }
