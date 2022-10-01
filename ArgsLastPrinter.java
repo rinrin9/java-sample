70681202bdaef45f55877d6ab5c5fcfbd2bf78c2
rinrin <r.chaaaaaan@gmail.com>
2022-10-01 15:33:50 +0900

    8-課題1
    Format

--- ArgsLastPrinter.java
+++ ArgsLastPrinter.java
@@ -1,10 +1,10 @@
-public class ArgsLastPrinter{
-  public static void main(String[] args) {
-    int i = args.length;
-    if (args.length == 0) {
-      System.out.println("なし");
-    }else{       
-      System.out.println(args[i-1]);
-    }
-  }
+public class ArgsLastPrinter {
+	public static void main(String[] args) {
+		int i = args.length;
+		if (args.length == 0) {
+			System.out.println("なし");
+		} else {
+			System.out.println(args[i - 1]);
+		}
+	}
 }
